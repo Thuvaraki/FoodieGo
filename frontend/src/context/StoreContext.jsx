2004a347494a5d7f8ddefb0f9cc13426796851fb
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-31 18:11:07 +0530

    Store Context

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,15 @@
+import react, { createContext } from "react";
+import { food_list } from "../assets/frontend_assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const contextValue = { food_list };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 );

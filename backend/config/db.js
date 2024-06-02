2c9466875d69adc0c98e167b3c9a99b91bba7811
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-06-02 08:47:51 +0530

    DB connect

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://FoodieGo:n4FiSNAJfdSkkfgN@cluster0.wyocwfe.mongodb.net/FoodieGoretryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => console.log("DB connected"));
+};

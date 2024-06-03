55d3a5c46aa6dbc1ba8ac4bac597e25d8ca35b17
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-06-03 15:35:07 +0530

    remove from cart, get cart backend

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -19,9 +19,31 @@ const addToCart = async (req, res) => {
 };
 
 //remove item from user cart
-const removeFromCart = async (req, res) => {};
+const removeFromCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed From Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 //fetch user cart data
-const getCart = async (req, res) => {};
+const getCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 export { addToCart, removeFromCart, getCart };

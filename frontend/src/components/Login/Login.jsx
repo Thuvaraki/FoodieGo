d31ae31e4b3aeb836d932d987afd305db7d6fc6c
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-06-03 10:15:33 +0530

    connect login,register backend frontend

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,12 +1,50 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Login.css";
 import { assets } from "../../assets/frontend_assets/assets";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
 const Login = ({ setShowLogin }) => {
+  const { url, setToken } = useContext(StoreContext);
+  // console.log(url);
+
   const [currState, setCurrState] = useState("SignUp");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setData((data) => ({ ...data, [e.target.name]: e.target.value }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    let newUrl = url;
+
+    if (currState === "Login") {
+      newUrl += "api/user/login";
+    } else {
+      newUrl += "api/user/register";
+    }
+
+    // console.log(newUrl);
+
+    const response = await axios.post(newUrl, data);
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLogin(false);
+    } else {
+      alert(response.data.message);
+    }
+  };
+
   return (
     <div className="login">
-      <form className="login-container">
+      <form className="login-container" onSubmit={onSubmitHandler}>
         <div className="login-title">
           <h2>{currState}</h2>
           <img onClick={() => setShowLogin(false)} src={assets.cross_icon} />
@@ -15,12 +53,35 @@ const Login = ({ setShowLogin }) => {
           {currState === "Login" ? (
             <></>
           ) : (
-            <input type="text" placeholder="Your name" required />
+            <input
+              onChange={onChangeHandler}
+              name="name"
+              value={data.name}
+              type="text"
+              placeholder="Your name"
+              required
+            />
           )}
-          <input type="email" placeholder="Your email" required />
-          <input type="password" placeholder="Password" required />
+          <input
+            onChange={onChangeHandler}
+            name="email"
+            value={data.email}
+            type="email"
+            placeholder="Your email"
+            required
+          />
+          <input
+            onChange={onChangeHandler}
+            name="password"
+            value={data.password}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </div>
-        <button>{currState === "SignUp" ? "Create account" : "Login"}</button>
+        <button type="submit">
+          {currState === "SignUp" ? "Create account" : "Login"}
+        </button>
         <div className="login-condition">
           <input type="checkbox" required />
           <p>By continuing, I agree to the terms of use & privacy policy</p>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,6 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000/";
+  const [token, setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -40,6 +42,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken,
   };
   return (
     <StoreContext.Provider value={contextValue}>

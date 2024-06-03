4027dba6478e3a2ac335523ea977ae7cf62e6dcb
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-06-03 21:16:01 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu.jsx/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
